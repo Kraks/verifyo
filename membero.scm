8dcbf6b169178404d68954eb06f5d69e9dc7eff5
Guannan Wei <guannanwei@outlook.com>
2019-05-14 21:36:45 -0400

    add mk

--- membero.scm
+++ membero.scm
@@ -4,3 +4,11 @@
          (conde
            [(== a x)]
            [(membero x d)])))
+
+(define (not-membero x xs)
+  (conde
+   [(== xs '())]
+   [(fresh (a d)
+           (== xs `(,a . ,d))
+           (=/= a x)
+           (not-membero x d))]))
