77a3809f8fb046299af5f298f21fc7192d648f4a
Guannan Wei <guannanwei@outlook.com>
2019-05-20 20:29:52 -0400

    reorganize

--- dpll/dpll.scm
+++ dpll/dpll.scm
@@ -1,52 +1,11 @@
 (load "../mk/mk.scm")
-(load "../membero.scm")
+(load "utils.scm")
 
 #|
 A formula is in CNF, i.e., a list of clauses.
 A clauses is a disjunction of literals, i.e., a list of literals.
 A literal is either a symbol, or a negation of a symbol (¬ x).
 |#
-
-(define-syntax listᵒ
-  (syntax-rules (← with)
-    ((_ ((x . xs) ← lst) (with v ...) rel ...)
-     (listᵒ ((x . xs) ← lst)
-            (fresh (v ...) rel ...)))
-    ((_ ((x . xs) ← lst) rel ...)
-     (conde
-      [(== lst '())]
-      [(fresh (x xs)
-              (== lst `(,x . ,xs))
-              rel ...)]))))
-
-(define (forall lst rel)
-  (listᵒ ((x . xs) ← lst)
-         (rel x)
-         (forall xs rel)))
-
-(define-syntax ∀
-  (syntax-rules (←)
-    ((_ (x ← xs) rel ...)
-     (forall xs (lambda (x) rel ...)))))
-
-(define-syntax ∃
-  (syntax-rules (←)
-    ((_ (x = (f xs ...)) rel ...)
-     (fresh (x) (f xs ... x) rel ...))
-    ((_ (x ← xs) rel ...)
-     (fresh (x) (∈ x xs) rel ...))
-    ((_ (x ...) rel ...)
-     (fresh (x ...) rel ...))))
-
-(define-syntax ∧
-  (syntax-rules ()
-    ((_ rel ...)
-     (fresh () rel ...))))
-
-(define-syntax ∨
-  (syntax-rules ()
-    ((_ (rel ...) ...)
-     (conde (rel ...) ...))))
 
 (define (emptyᵒ l) (== l '()))
 
@@ -66,28 +25,6 @@ A literal is either a symbol, or a negation of a symbol (¬ x).
 (define (formulaᵒ f) (forall f clauseᵒ))
 
 (define (modelᵒ m) (forall m litᵒ))
-
-(define (∩ x y z)
-  (∨ [(emptyᵒ x) (emptyᵒ z)]
-     [(emptyᵒ y) (emptyᵒ z)]
-     [(fresh (a d z^)
-             (== `(,a . ,d) x)
-             (∨ [(∈ a y)
-                 (∩ d y z^)
-                 (== z `(,a . ,z^))]
-                [(∉ a y)
-                 (∩ d y z)]))]))
-
-(define (∪ x y z)
-  (∨ [(emptyᵒ x) (== y z)]
-     [(emptyᵒ y) (== x z)]
-     [(fresh (a d z^)
-             (== x `(,a . ,d))
-             (conde
-              [(∈ a y) (∪ d y z)]
-              [(∉ a y)
-               (∪ d y z^)
-               (== z `(,a . ,z^))]))]))
 
 (define (c/atomsᵒ c as)
   (∨ [(emptyᵒ c) (emptyᵒ as)]
@@ -136,48 +73,6 @@ A literal is either a symbol, or a negation of a symbol (¬ x).
 (define (f/⊭ m f)
   (∃ (c ← f) (c/⊭ m c)))
 
-;; Split list pxq at the the first occorence of x.
-(define (splitᵒ pxq p x q)
-  (∃ (a d p^)
-     (== pxq `(,a . ,d))
-     (∨ [(== a x)
-         (emptyᵒ p)
-         (== q d)]
-        [(=/= a x)
-         (splitᵒ d p^ x q)
-         (== p `(,a . ,p^))])))
-
-(define (rem-dupᵒ xs ys)
-  (∨ [(emptyᵒ xs) (emptyᵒ ys)]
-     [(∃ (a d ys^)
-         (== xs `(,a . ,d))
-         (∨ [(∈ a d) (rem-dupᵒ d ys)]
-            [(∉ a d)
-             (rem-dupᵒ d ys^)
-             (== ys `(,a . ,ys^))]))]))
-
-(define (appendᵒ l s out)
-  (∨ [(emptyᵒ l) (== s out)]
-     [(∃ (a d res)
-         (== `(,a . ,d) l)
-         (== `(,a . ,res) out)
-         (appendᵒ d s res))]))
-
-(define (flattenᵒ xss xs)
-  (∨ [(emptyᵒ xss) (emptyᵒ xs)]
-     [(∃ (a d res)
-         (== xss `(,a . ,d))
-         (flattenᵒ d res)
-         (appendᵒ a res xs))]))
-
-(define (⊆* ∈)
-  (lambda (xs ys)
-    (∨ [(emptyᵒ xs)]
-       [(∃ (a d)
-           (== xs `(,a . ,d))
-           (∈ a ys)
-           ((⊆* ∈) d ys))])))
-
 (define (⊆ xs ys) ((⊆* (lambda (a m) (↓ m a))) xs ys))
 
 ;; should be the final state: all the variables are assigned.
@@ -188,34 +83,10 @@ A literal is either a symbol, or a negation of a symbol (¬ x).
      (⊆ m vars)
      (⊆ vars m)))
 
-(define (removeᵒ xs x ys)
-  (∨ [(emptyᵒ xs) (emptyᵒ ys)]
-     [(∃ (a d ys^)
-         (== xs `(,a . ,d))
-         (∨ [(== a x) (removeᵒ d x ys)]
-            [(=/= a x)
-             (removeᵒ d x ys^)
-             (== ys `(,a . ,ys^))]))]))
-
 (define (c/unitpropᵒ c x c^)
   (∨ [(∈ x c) (== c^ #t)]
      [(∃ (¬x ← c) (negᵒ x ¬x) (∉ x c) (removeᵒ c ¬x c^))]
      [(↑ c x) (== c c^)]))
-
-;; mapfilter takes a list `xs`, a predicate relation `p`, and transformer relation `f`,
-;; then transforms every element `x` in `xs`, and only keeps those transformed values who
-;; satifies `p`.
-(define (mapfilterᵒ xs p f ys)
-  (∨ [(emptyᵒ xs) (emptyᵒ ys)]
-     [(∃ (a a^ d r ys^)
-         (== xs `(,a . ,d))
-         (f a a^)
-         (p a^ r)
-         (∨ [(== r #t)
-             (mapfilterᵒ d p f ys^)
-             (== ys `(,a^ . ,ys^))]
-            [(== r #f)
-             (mapfilterᵒ d p f ys)]))]))
 
 ;; Unit propagation: applying x to f, and obtaining f^.
 ;; Note: an empty disjunction is false; an empty conjunction is true
---
+++ dpll/utils.scm
@@ -0,0 +1,149 @@
+(load "../mk/mk.scm")
+
+(define (membero x xs)
+  (fresh (a d)
+         (== `(,a . ,d) xs)
+         (conde
+           [(== a x)]
+           [(membero x d)])))
+
+(define (not-membero x xs)
+  (conde
+   [(== xs '())]
+   [(fresh (a d)
+           (== xs `(,a . ,d))
+           (=/= a x)
+           (not-membero x d))]))
+
+(define (∈ x m) (membero x m))
+(define (∉ x m) (not-membero x m))
+
+(define-syntax listᵒ
+  (syntax-rules (← with)
+    ((_ ((x . xs) ← lst) (with v ...) rel ...)
+     (listᵒ ((x . xs) ← lst)
+            (fresh (v ...) rel ...)))
+    ((_ ((x . xs) ← lst) rel ...)
+     (conde
+      [(== lst '())]
+      [(fresh (x xs)
+              (== lst `(,x . ,xs))
+              rel ...)]))))
+
+(define (forall lst rel)
+  (listᵒ ((x . xs) ← lst)
+         (rel x)
+         (forall xs rel)))
+
+(define-syntax ∀
+  (syntax-rules (←)
+    ((_ (x ← xs) rel ...)
+     (forall xs (lambda (x) rel ...)))))
+
+(define-syntax ∃
+  (syntax-rules (←)
+    ((_ (x = (f xs ...)) rel ...)
+     (fresh (x) (f xs ... x) rel ...))
+    ((_ (x ← xs) rel ...)
+     (fresh (x) (∈ x xs) rel ...))
+    ((_ (x ...) rel ...)
+     (fresh (x ...) rel ...))))
+
+(define-syntax ∧
+  (syntax-rules ()
+    ((_ rel ...)
+     (fresh () rel ...))))
+
+(define-syntax ∨
+  (syntax-rules ()
+    ((_ (rel ...) ...)
+     (conde (rel ...) ...))))
+
+(define (∩ x y z)
+  (∨ [(emptyᵒ x) (emptyᵒ z)]
+     [(emptyᵒ y) (emptyᵒ z)]
+     [(fresh (a d z^)
+             (== `(,a . ,d) x)
+             (∨ [(∈ a y)
+                 (∩ d y z^)
+                 (== z `(,a . ,z^))]
+                [(∉ a y)
+                 (∩ d y z)]))]))
+
+(define (∪ x y z)
+  (∨ [(emptyᵒ x) (== y z)]
+     [(emptyᵒ y) (== x z)]
+     [(fresh (a d z^)
+             (== x `(,a . ,d))
+             (conde
+              [(∈ a y) (∪ d y z)]
+              [(∉ a y)
+               (∪ d y z^)
+               (== z `(,a . ,z^))]))]))
+
+;; Split list pxq at the the first occorence of x.
+(define (splitᵒ pxq p x q)
+  (∃ (a d p^)
+     (== pxq `(,a . ,d))
+     (∨ [(== a x)
+         (emptyᵒ p)
+         (== q d)]
+        [(=/= a x)
+         (splitᵒ d p^ x q)
+         (== p `(,a . ,p^))])))
+
+(define (rem-dupᵒ xs ys)
+  (∨ [(emptyᵒ xs) (emptyᵒ ys)]
+     [(∃ (a d ys^)
+         (== xs `(,a . ,d))
+         (∨ [(∈ a d) (rem-dupᵒ d ys)]
+            [(∉ a d)
+             (rem-dupᵒ d ys^)
+             (== ys `(,a . ,ys^))]))]))
+
+(define (appendᵒ l s out)
+  (∨ [(emptyᵒ l) (== s out)]
+     [(∃ (a d res)
+         (== `(,a . ,d) l)
+         (== `(,a . ,res) out)
+         (appendᵒ d s res))]))
+
+(define (flattenᵒ xss xs)
+  (∨ [(emptyᵒ xss) (emptyᵒ xs)]
+     [(∃ (a d res)
+         (== xss `(,a . ,d))
+         (flattenᵒ d res)
+         (appendᵒ a res xs))]))
+
+(define (⊆* ∈)
+  (lambda (xs ys)
+    (∨ [(emptyᵒ xs)]
+       [(∃ (a d)
+           (== xs `(,a . ,d))
+           (∈ a ys)
+           ((⊆* ∈) d ys))])))
+
+(define (removeᵒ xs x ys)
+  (∨ [(emptyᵒ xs) (emptyᵒ ys)]
+     [(∃ (a d ys^)
+         (== xs `(,a . ,d))
+         (∨ [(== a x) (removeᵒ d x ys)]
+            [(=/= a x)
+             (removeᵒ d x ys^)
+             (== ys `(,a . ,ys^))]))]))
+
+;; mapfilter takes a list `xs`, a predicate relation `p`, and transformer relation `f`,
+;; then transforms every element `x` in `xs`, and only keeps those transformed values who
+;; satifies `p`.
+(define (mapfilterᵒ xs p f ys)
+  (∨ [(emptyᵒ xs) (emptyᵒ ys)]
+     [(∃ (a a^ d r ys^)
+         (== xs `(,a . ,d))
+         (f a a^)
+         (p a^ r)
+         (∨ [(== r #t)
+             (mapfilterᵒ d p f ys^)
+             (== ys `(,a^ . ,ys^))]
+            [(== r #f)
+             (mapfilterᵒ d p f ys)]))]))
+
